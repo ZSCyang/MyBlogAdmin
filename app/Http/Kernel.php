8e626f99dd2e92aac6776f58037a185ffd4edd38
ZSCyang <yjt_it@163.com>
2020-04-14 14:57:43 +0800

    测试接口

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,6 +59,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'rbac' => \App\Http\Middleware\RbacAuth::class,   //后台BBAC权限控制
-        'jwt_auth' => JwtMiddleware::class,
+        /*'jwt_auth' => JwtMiddleware::class,*/
     ];
 }
